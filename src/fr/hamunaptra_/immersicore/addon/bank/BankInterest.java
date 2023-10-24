c3d08ff4aac41669fa741566a8f762b33c162dfe
Hamunaptra89 <88455685+Hamunaptra89@users.noreply.github.com>
2023-10-24 15:03:44 +0200

    Update BankInterest.java

--- src/fr/hamunaptra_/immersicore/addon/bank/BankInterest.java
+++ src/fr/hamunaptra_/immersicore/addon/bank/BankInterest.java
@@ -9,26 +9,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class BankInterest {
-
-    public static void run() {
-        Bukkit.getServer().getScheduler().runTaskTimer(Main.getInstance(), () -> {
+    
+  public static void run() {
+    Bukkit.getServer().getScheduler().runTaskTimer((Plugin)Main.getInstance(), new Runnable() {
+          public void run() {
             String path = "Bank.Interest.";
-
-            if (Bank.getBoolean(path + "Enable")) {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    DataManager Data = new DataManager(p);
-                    Data.removeInterestTime(1);
-
-                    if (Data.getInterestTime() <= 0 && Data.getBalance() >= Bank.getDouble(path + "Minimum")) {
-                        double amount = Data.getBalance() * Bank.getDouble(path + "Percent");
-                        Data.deposit(amount);
-
-                        Color Color = new Color(p);
-                        p.sendMessage(Color.set(Bank.getString(path + "Message.Get")).replaceAll("%interest%", String.valueOf(amount)));
-                    }
-                    Data.setInterestTime(Bank.getInt(path + "Time"));
-                }
-            }
-        }, 20, 20);
-    }
+            if (Bank.getBoolean(path + "Enable"))
+              for (Player p : Bukkit.getOnlinePlayers()) {
+                DataManager Data = new DataManager(p);
+                Data.InterestCooldownRemove(1);
+                if (Data.getLastInterest() <= 0 && Data.getBalance() >= Bank.getDouble(path + "Minimum")) {
+                  double amount = Data.getBalance() * Bank.getDouble(path + "Percent");
+                  Data.deposit(amount);
+                  p.sendMessage(Color.set(Bank.getString(path + "Message.Get")).replaceAll("%interest%", String.valueOf(amount)));
+                } 
+                Data.setLastInterest(Bank.getInt(path + "Time"));
+              }  
+          }
+        }20L, 20L);
+  }
 }
